33d406e8d7e2e87c9bc5c9c0b4f351c95bc36d7f
Islomjons <mukhammadalievislomjon@gmail.com>
2022-07-06 17:52:30 +0500

    Java

---
+++ main.js
@@ -0,0 +1,15 @@
+var myByudjet = prompt ("Summani kriting");
+
+var dollar = 9433.34;
+
+var Euro = 10354.03;
+
+var money = Math.floor (dollar * 750) + (Euro * 120);
+
+if (myByudjet >= money){
+    console.log("Oq yol Alisher");
+    alert("Oq yol ALisher");
+}else{
+    console.log("Ozgina sobr qilishingiz kerak Alisher");
+    alert("Ozgina sobr qilishingiz kerak Alisher")
+}
